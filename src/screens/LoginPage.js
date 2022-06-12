39b25734fa33156525e9e015c7bff1ba44e69a8b
Anonymous747 <kobrinets_m@mail.ru>
2022-06-13 00:26:18 +0300

    Login screen

--- src/screens/LoginPage.js
+++ src/screens/LoginPage.js
@@ -14,62 +14,58 @@ import {PalleteColor} from '../general/PalleteColor';
 import {SeparatedText} from '../widgets/login/SeparatedText';
 import {IconsRaw} from '../widgets/login/IconsRaw';
 import {GradientButton} from '../widgets/common/GradientButton';
-import LinearGradient from 'react-native-linear-gradient';
+import {BackgroundLinearGradient} from '../widgets/common/BackgroundLinearGradient';
+import {ModalSheet} from '../widgets/common/ModalSheet';
 
 const LoginPage = () => {
   const navigation = useNavigation();
 
   return (
-    <View style={styles.mainView}>
+    <BackgroundLinearGradient>
       <StatusBar translucent backgroundColor="transparent" />
-      <LinearGradient
-        colors={[PalleteColor.Red, PalleteColor.Orange]}
-        start={{x: 0, y: 0}}
-        end={{x: 1, y: 1}}
-        style={styles.headerGradient}>
+      <View style={styles.logoView}>
         <Text style={styles.logoText}>Eco Textile</Text>
-        <View style={styles.headerView}>
-          <View style={styles.whiteLineView} />
-        </View>
-      </LinearGradient>
-
-      <View style={styles.bodyView}>
-        <Text style={styles.header}>Welcome back!</Text>
-        <Text style={styles.subHeader}>
-          Please login with your email address and password to continue.
-        </Text>
-        <TextInput placeholder="Email address" style={styles.textInput} />
-        <TextInput placeholder="Password" style={styles.textInput} />
-        <TouchableWithoutFeedback>
-          <Text style={styles.forgotStyle}>Forgot password?</Text>
-        </TouchableWithoutFeedback>
-        <GradientButton colors={[]} buttonText="Sign In" />
-        <SeparatedText param={'or'} />
-        <IconsRaw icons={[]} />
-        <View style={styles.footerView}>
-          <Text style={styles.dontHaveAccountText}>Don't have an account?</Text>
-          <TouchableOpacity onPress={() => navigation.navigate('SignUp')}>
-            <Text style={styles.signUpText}>Sign up now!</Text>
-          </TouchableOpacity>
-        </View>
       </View>
-    </View>
+      <ModalSheet>
+        <View style={styles.bodyView}>
+          <Text style={styles.header}>Welcome back!</Text>
+          <Text style={styles.subHeader}>
+            Please login with your email address and password to continue.
+          </Text>
+          <TextInput placeholder="Email address" style={styles.textInput} />
+          <TextInput placeholder="Password" style={styles.textInput} />
+          <TouchableWithoutFeedback>
+            <Text style={styles.forgotStyle}>Forgot password?</Text>
+          </TouchableWithoutFeedback>
+          <GradientButton colors={[]} buttonText="Sign In" />
+          <SeparatedText param={'or'} />
+          <IconsRaw icons={[]} />
+          <View style={styles.footerView}>
+            <Text style={styles.dontHaveAccountText}>
+              Don't have an account?
+            </Text>
+            <TouchableOpacity onPress={() => navigation.navigate('SignUp')}>
+              <Text style={styles.signUpText}>Sign up now!</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ModalSheet>
+    </BackgroundLinearGradient>
   );
 };
 
 const styles = StyleSheet.create({
   // Texts
   logoText: {
-    color: '#FFF',
+    color: PalleteColor.White,
     fontWeight: 'bold',
-    marginTop: '16%',
     fontSize: 50,
   },
   header: {
-    fontSize: 26,
-    marginBottom: 4,
     color: '#000',
     fontWeight: 'bold',
+    fontSize: 26,
+    marginTop: '10%',
   },
   subHeader: {
     fontSize: 16,
@@ -99,18 +95,7 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: 'bold',
   },
-
-  // Gradients
-  headerGradient: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '30%',
-  },
   // Views
-  mainView: {
-    height: '100%',
-    backgroundColor: '#FFF',
-  },
   bodyView: {
     flex: 1,
     backgroundColor: 'white',
@@ -123,14 +108,10 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     flex: 1,
   },
-  whiteLineView: {
-    width: '100%',
-    height: 20,
-    backgroundColor: '#FFF',
-    position: 'absolute',
-    bottom: -1,
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,
+  logoView: {
+    justifyContent: 'center',
+    alignSelf: 'center',
+    height: 200,
   },
   headerView: {
     height: '100%',
