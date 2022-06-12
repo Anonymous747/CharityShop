5c6db93883850faf2e22aeafaa9ceb3a6195405f
Anonymous747 <kobrinets_m@mail.ru>
2022-06-12 15:49:04 +0300

    Icon pack

---
+++ src/widgets/common/Index.js
@@ -0,0 +1,5 @@
+import { BackArrow } from "./BackArrow";
+import { CustomLinearGradient } from "./CustomLinearGradient";
+import { GradientButton } from "./GradientButton";
+
+export { BackArrow, CustomLinearGradient, GradientButton, }
